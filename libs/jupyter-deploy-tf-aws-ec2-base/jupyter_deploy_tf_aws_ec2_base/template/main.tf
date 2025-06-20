1f3be59df036c835ab24f0f02bab639ec22683d4
Jonathan Guinegagne <jggg@amazon.com>
2025-06-20 14:35:09 +0000

    fix: redirection infinite loop

--- libs/jupyter-deploy-tf-aws-ec2-base/jupyter_deploy_tf_aws_ec2_base/template/main.tf
+++ libs/jupyter-deploy-tf-aws-ec2-base/jupyter_deploy_tf_aws_ec2_base/template/main.tf
@@ -341,7 +341,7 @@ locals {
   docker_compose_file = templatefile("${path.module}/docker-compose.yml.tftpl", {
     oauth_provider           = var.oauth_provider
     full_domain              = local.full_domain
-    auth_regex               = "^https://auth\\\\.${replace(local.full_domain, ".", "\\\\.")}/?$"
+    auth_regex               = "^https://auth\\\\.${replace(local.full_domain, ".", "\\\\.")}/(.*)"
     github_client_id         = var.oauth_app_client_id
     aws_region               = data.aws_region.current.name
     allowed_github_usernames = join(",", [for username in var.oauth_allowed_usernames : "${username}"])
