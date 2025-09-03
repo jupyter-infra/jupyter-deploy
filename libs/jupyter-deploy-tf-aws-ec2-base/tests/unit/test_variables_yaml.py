617c6199d351f8894955eee55fb61856af0556e8
Jonathan Guinegagne <jggg@amazon.com>
2025-09-03 17:00:38 +0000

    fix: merge conflict

--- libs/jupyter-deploy-tf-aws-ec2-base/tests/unit/test_variables_yaml.py
+++ libs/jupyter-deploy-tf-aws-ec2-base/tests/unit/test_variables_yaml.py
@@ -3,8 +3,6 @@ from pathlib import Path
 
 import hcl2
 import yaml
-
-from jupyter_deploy.variables_config import JupyterDeployVariablesConfigV1
 from jupyter_deploy.handlers import base_project_handler
 
 from jupyter_deploy_tf_aws_ec2_base.template import TEMPLATE_PATH
@@ -47,10 +45,6 @@ class TestVariablesYaml(unittest.TestCase):
                     tf_variables[var_name] = var_config
 
             TestVariablesYaml.TF_VARIABLES = tf_variables
-
-    def test_variables_yaml_parsable_by_jupyter_deploy(self) -> None:
-        variables_config = JupyterDeployVariablesConfigV1.model_validate(self.VARIABLES_CONFIG)
-        self.assertIsInstance(variables_config, JupyterDeployVariablesConfigV1)
 
     def test_all_keys_are_present(self) -> None:
         self.assertIn("required", self.VARIABLES_CONFIG)
